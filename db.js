4c244242714377491ebaff6ad6c6ef4584b348b9
SzDevBR <81070056+SzDevBR@users.noreply.github.com>
2023-07-29 23:24:37 -0300

    Update db.js

--- db.js
+++ db.js
@@ -2,19 +2,20 @@
 const mongoose = require('mongoose');
 
 // URL do banco de dados MongoDB
-const MONGODB_URI = 'mongodb+srv://inderux:inderux@cluster0.yuzl0.mongodb.net/?retryWrites=true&w=majority';
+const mongoURI = 'mongodb+srv://inderux:inderux@cluster0.yuzl0.mongodb.net/?retryWrites=true&w=majority';
 
-// Configuração e conexão com o MongoDB
-mongoose
-  .connect(MONGODB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => {
-    console.log('Conectado ao MongoDB');
-  })
-  .catch((error) => {
-    console.error('Erro ao conectar ao MongoDB:', error.message);
-  });
+// Configuração do MongoDB
+mongoose.connect(mongoURI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+});
 
-module.exports = mongoose;
+// Verificar se a conexão foi estabelecida com sucesso
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'Erro de conexão com o MongoDB:'));
+db.once('open', () => {
+  console.log('Conexão com o MongoDB estabelecida com sucesso!');
+});
+
+module.exports = db;
